72d587d79812ec8c17d913ea697f110c46c38ae8
Don McCaughey <don@donm.cc>
2025-01-05 15:48:31 -0800

    Clean up Xcode generated code.

--- Mutabit/AppDelegate.swift
+++ Mutabit/AppDelegate.swift
@@ -1,29 +1,17 @@
-//
-//  AppDelegate.swift
-//  Mutabit
-//
-//  Created by Don McCaughey on 1/5/25.
-//
-
 import Cocoa
+
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
     }
 
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         return true
     }
 
-
 }
-
